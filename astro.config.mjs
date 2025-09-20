86115980a2cc9e9e9b79cedca666c1becbba3c30
LukeRocha <lucas.ar93@gmail.com>
2025-09-20 15:08:35 -0300

    Change prettier config to run on astro files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import astroIcon from "astro-icon";
 
 export default defineConfig({
+  integrations: [
+    astroIcon({
+      iconSets: ["material-symbols"],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
