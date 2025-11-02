d4338bdbe75e95a09a5278b4e6dee13818829a36
LukeRocha <lucas.ar93@gmail.com>
2025-11-02 18:43:40 -0300

    Add netlify ssr config due to random hero images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import tailwindcss from '@tailwindcss/vite';
 import astroIcon from 'astro-icon';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: netlify(),
   integrations: [
-    astroIcon({
-      iconSets: ['material-symbols'],
-    }),
+    astroIcon({ iconSets: ['material-symbols'] }),
   ],
   vite: {
     plugins: [tailwindcss()],
